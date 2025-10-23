a362d64f18f3a243ef08629336bae7e9f7bdff10
Sota Tsunemine <motarou.engineer@gmail.com>
2025-10-23 18:14:30 +0900

    feat: sourcesにタイルの情報を埋め込めるようにした

--- transform_style.js
+++ transform_style.js
@@ -6,10 +6,16 @@ const input = JSON.parse(fs.readFileSync("style.json", "utf-8"));
 delete input.glyphs; // glyphs削除
 delete input.sprite; // sprite削除
 
-// sourcesのurlを""に変更
-input.sources.openmaptiles.url = "";
-
 input.layers = input.layers.filter((layer) => layer.type !== "symbol");
+
+input.sources = {
+  openmaptiles: {
+    type: "vector",
+    tiles: ["https://map-tile-server.pages.dev/tiles/{z}/{x}/{y}.pbf"],
+    attribution:
+      '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, © <a href="https://openmaptiles.org/">OpenMapTiles</a>',
+  },
+},
 
 fs.writeFileSync("style.no-glyphs.json", JSON.stringify(input, null, 2));
 console.log("✅ glyphs と symbol レイヤーを削除しました。");
